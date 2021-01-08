b94ae88eb2d64575564bbe39fc2e782fe24ad79d
Juan Jose <juanjohupuj@javerianacali.edu.co>
2021-01-08 10:40:03 -0500

    education aded

--- personal_portfolio/portfolio/views.py
+++ personal_portfolio/portfolio/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Project
+from education.models import Education
 
 # Create your views here.
 def home(request):
-    return render(request,"portfolio/home.html")
+    education = Education.objects.all()
+    return render(request,"portfolio/home.html", {"education": education})
 
 def portfolio(request):
     fetch = Project.objects.all()
